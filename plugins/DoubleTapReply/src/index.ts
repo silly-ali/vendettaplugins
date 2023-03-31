0acaf77dfbc8f56e55c425c58ba44876180df3fd
eszgit <44556757+eszgit@users.noreply.github.com>
2023-03-30 21:59:28 -0400

    Add files via upload

--- plugins/DoubleTapReply/src/index.ts
+++ plugins/DoubleTapReply/src/index.ts
@@ -1,12 +1,46 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { Plugin } from '@vendetta/entities';
+// @ts-ignore
+import { getModule, getByProps, MessageStore, ChannelStore } from '@vendetta/metro';
 
-export default {
-    onLoad: () => {
-        logger.log("Double Tap Reply has started.");
-    },
-    onUnload: () => {
-        logger.log("Double Tap Reply has stopped.");
-    },
-    settings: Settings,
+type FilterOptions = {
+    exports?: boolean;
+    default?: false;
+} | {
+    exports?: true;
+    default?: true;
+};
+
+export default class DoubleTapReply extends Plugin {
+    private getByName(defaultName: string, options?: FilterOptions) {
+        return getModule(m => m?.default?.name === defaultName, options);
+    }
+    public async start() {
+        const ReplyManager = getByProps("createPendingReply")
+
+        const Chat = this.getByName("Chat")
+
+        let chatTapCount = 0;
+        let oldTappedMessage = "";
+        let timeoutId;
+
+        Chat.default.defaultProps.onDoubleTapMessage = (arg) => {
+            chatTapCount++;
+            
+            console.log("MessageTap",arg.nativeEvent,chatTapCount)
+            timeoutId = setTimeout(()=>{chatTapCount = 0},300);
+            if(chatTapCount == 2){
+                clearTimeout(timeoutId)
+                const nativeEvent = arg.nativeEvent;
+                if(oldTappedMessage == nativeEvent.messageId){
+                    ReplyManager.createPendingReply({
+                        channel: ChannelStore.getChannel(nativeEvent.channelId),
+                        message: MessageStore.getMessage(nativeEvent.channelId,nativeEvent.messageId),
+                        shouldMention: true
+                    })
+                }
+                chatTapCount = 0
+            }
+            oldTappedMessage = arg.nativeEvent.messageId;
+        }
+    }
 }
